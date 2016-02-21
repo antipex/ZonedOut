cb79fc301a4a6612d150a2980c337ee9e26f9ed7
kylerohr <kyle@antipex.com>
2016-02-21 14:12:19 -0800

    New add user UI

--- ZonedOut/AddUserController.swift
+++ ZonedOut/AddUserController.swift
@@ -2,45 +2,25 @@
 //  AddUserController.swift
 //  ZonedOut
 //
-//  Created by Kyle Rohr on 20/10/2015.
-//  Copyright © 2015 Kyle Rohr. All rights reserved.
+//  Created by Kyle Rohr on 21/02/2016.
+//  Copyright © 2016 Kyle Rohr. All rights reserved.
 //
 
 import UIKit
 import SVProgressHUD
 
-class AddUserController: UIViewController {
-
-    @IBOutlet weak var usernameField: UITextField?
-    @IBOutlet weak var passwordField: UITextField?
-    @IBOutlet weak var emailField: UITextField?
-    @IBOutlet weak var firstNameField: UITextField?
-    @IBOutlet weak var lastNameField: UITextField?
+class AddUserController: EditUserController {
 
     override func viewDidLoad() {
+        mode = .Add
+
         super.viewDidLoad()
-
-        if let navigationBar = navigationController?.navigationBar {
-            navigationBar.barTintColor = UIColor(hex: 0x235da2)
-            navigationBar.barStyle = .BlackTranslucent
-            navigationBar.tintColor = UIColor.whiteColor()
-        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // MARK: - Actions
 
-    @IBAction func createAccount() {
-
-        guard let usernameField = usernameField, let passwordField = passwordField, let emailField = emailField, let firstNameField = firstNameField, let lastNameField = lastNameField else {
-
-            return
-        }
-
-        API.addUser(usernameField.text!, password: passwordField.text!, email: emailField.text!, firstName: firstNameField.text!, lastName: lastNameField.text!) { response in
+    override func passedValidation() {
+        API.addUser(userData.username, password: userData.password, email: userData.email, firstName: userData.firstName, lastName: userData.lastName) { response in
 
             switch response.result {
             case .Success(let rawJSON):
@@ -51,7 +31,7 @@ class AddUserController: UIViewController {
 
                 self.dismissViewControllerAnimated(true, completion: nil)
             case .Failure(let error):
-                let alert = UIAlertController(title: "Error Creating Account", message: error.localizedDescription, preferredStyle: .Alert)
+                let alert = UIAlertController(title: "Could not create account", message: error.localizedDescription, preferredStyle: .Alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
                 self.presentViewController(alert, animated: true, completion: nil)
             }
---
+++ ZonedOut/TextFieldCell.swift
@@ -0,0 +1,31 @@
+//
+//  TextFieldCell.swift
+//  ZonedOut
+//
+//  Created by Kyle Rohr on 21/02/2016.
+//  Copyright © 2016 Kyle Rohr. All rights reserved.
+//
+
+import UIKit
+
+protocol TextFieldCellDelegate {
+    func textFieldCell(cell: TextFieldCell, didUpdateValue value: String?)
+}
+
+class TextFieldCell: UITableViewCell, UITextFieldDelegate {
+
+    var delegate: TextFieldCellDelegate?
+
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+
+    // MARK: - UITextFieldDelegate
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        delegate?.textFieldCell(self, didUpdateValue: textField.text)
+    }
+
+}
